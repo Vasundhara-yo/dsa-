57db60e6f815491a7d848f2ec5d532008ab28a73
Vasundhara-yo <ivasundhara25@gmail.com>
2026-06-28 20:24:37 +0530

    Time: 3 ms (76.15%) | Memory: 22 MB (89.44%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -5,22 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    diameter = 0  
+    def calculateHeight(self, root):  
+        if not root:
+            return 0
+        leftHeight = self.calculateHeight(root.left)
+        rightHeight = self.calculateHeight(root.right)
+        self.diameter = max(self.diameter, leftHeight + rightHeight)   
+        return 1 + max(leftHeight, rightHeight)
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        diameter = 0
-
-        def height(node):
-            nonlocal diameter
-
-            if not node:
-                return 0
-
-            left = height(node.left)
-            right = height(node.right)
-
-            diameter = max(diameter, left + right)
-
-            return 1 + max(left, right)
-
-        height(root)
-        return diameter
         
+        self.diameter = 0
+        self.calculateHeight(root)
+        return self.diameter
